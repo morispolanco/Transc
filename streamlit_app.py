ca8f32481c461b9a834b4a4de96261b3af560f12
morispolanco <morispolanco@gmail.com>
2023-10-26 17:39:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
 import streamlit as st
+import sounddevice as sd 
 from deepgram import Deepgram
 import asyncio
-import aiohttp
+import time
 
-st.title('Transcripción de audio en vivo') 
+st.title('Transcripción de voz en vivo')
 
-# Tu clave API de Deepgram
+# Tu clave API de Deepgram 
 DEEPGRAM_API_KEY = '887355a9368a2b55cbb723a9b735af03f618ed6c'  
 
-# URL del audio en vivo a transcribir 
-URL = 'http://stream.live.vc.bbcmedia.co.uk/bbc_world_service'   
+
+# ID del micrófono a usar
+mic_id = 1  
 
 async def transcribe():
     deepgram = Deepgram(DEEPGRAM_API_KEY)
@@ -18,7 +20,9 @@ async def transcribe():
         deepgram_live = await deepgram.transcription.live({
           'smart_format': True,
           'interim_results': False,
-          'language': 'en-US',
+          'language': 'es-ES',
+          'sample_rate': 44100,
+          'channels': 1,
           'model': 'nova',
         })
     except Exception as e:
@@ -26,23 +30,26 @@ async def transcribe():
         return
 
     deepgram_live.registerHandler(deepgram_live.event.TRANSCRIPT_RECEIVED, show_transcript)
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(URL) as audio:
-            while True:
-                data = await audio.content.readany()
-                deepgram_live.send(data)
-                
-                if not data:
-                    break
-                    
+
+    with sd.RawInputStream(samplerate=44100, blocksize = 8000, device=mic_id, dtype='int16', 
+                        channels=1, callback=get_audio):
+        st.write('#' * 80)
+        st.write('Habla ahora para transcribir tu voz')
+        st.write('#' * 80)
+
+        while True:
+            time.sleep(0.1)    
+
     await deepgram_live.finish()
-    
+
+def get_audio(indata, frames, time, status):
+  deepgram_live.send(indata.tobytes())
+
 def show_transcript(transcript):
     try:
         text = transcript['channel']['alternatives'][0]['transcript']
         st.write(text)
     except:
-        st.warning('No se pudo acceder al texto transcripto')     
+        st.warning('No se pudo acceder al texto transcripto')
 
-asyncio.run(transcribe())
+asyncio.run(transcribe())  
