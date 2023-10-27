a47c0212c4761067c7bf1e9835c0bdb7a829a689
morispolanco <morispolanco@gmail.com>
2023-10-26 18:03:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,32 +8,32 @@ DEEPGRAM_API_KEY = '887355a9368a2b55cbb723a9b735af03f618ed6c'
 
 @st.cache
 def get_deepgram_client():
-    return Deepgram(DEEPGRAM_API_KEY)
+  return Deepgram(DEEPGRAM_API_KEY)
 
-audio_file = st.file_uploader("Sube un archivo de audio (M4A, MP3, WAV)", type=['m4a', 'mp3', 'wav'])
+audio_file = st.file_uploader("Sube un archivo M4A", type=['m4a'])
 
 if audio_file is not None:
 
-    dg_client = get_deepgram_client()
-    
-    audio_bytes = audio_file.read()
+  dg_client = get_deepgram_client()
 
-    try:
-        response = dg_client.transcription.sync_prerecorded(
-            {'buffer': audio_bytes, 'mimetype': audio_file.type},
-            {
-                'language': 'es-ES',
-                'smart_format': True
-            }
-        )
-    except Exception as e:
-        st.error(f"Error al transcribir el audio: {e}")
-        st.stop()
+  audio_bytes = audio_file.read()
 
-    st.write("Transcripción:")
-    st.write(response['results']['channels'][0]['alternatives'][0]['transcript'])
-    
-    st.json(response)
+  try:
+    response = dg_client.transcription.sync_prerecorded(
+      {'buffer': audio_bytes, 'mimetype': audio_file.type},
+      {
+        'language': 'es-ES',
+        'smart_format': True  
+      }
+    )
+  except Exception as e:
+    st.error(f"Error al transcribir el audio: {e}")
+    st.stop()
+
+  st.write("Transcripción:")
+  st.write(response['results']['channels'][0]['alternatives'][0]['transcript'])
+
+  st.json(response)
 
 else:
-    st.write("Sube un archivo de audio para comenzar")
+  st.write("Sube un archivo M4A para comenzar") 
